60a86c5d02af6198566e977f349f57ce6ba03148
NGU <jxdngu@gmail.com>
2023-04-19 14:53:46 +0800

    增加Linear的模型权重文件

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ config = {
     "data_min": "data/min.npy",
     "data_max": "data/max.npy",
     "data_path": "data/1-s2.0-S0950061822007656-mmc1.xlsx",
-    "ntype": "Conv",
+    "ntype": "Linear",
     "weights_path": "weights",
     "lr0": 0.001,
     "batch_size": 64,
     "epochs": 500,
-    "ckpt": "weights/Conv/best.pt"
+    "ckpt": "weights/Linear/best.pt"
 }
